55d22ccaafa49e365a3b6f85359a3c2672c9e586
pherphepheni <osorio.f@outlook.es>
2019-10-30 11:30:13 -0300

    Updates

---
+++ precache-manifest.b7053003bdb23f2ac7a78e16922dbd7f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a7fca364caabadd4c151d60eb37331f",
+    "url": "/pixaapp/index.html"
+  },
+  {
+    "revision": "566e6cde4db6b3938c0f",
+    "url": "/pixaapp/static/js/2.7cc8251f.chunk.js"
+  },
+  {
+    "revision": "9998685bdfb8c7a07a27",
+    "url": "/pixaapp/static/js/main.a32603cb.chunk.js"
+  },
+  {
+    "revision": "623b42f815ce55753656",
+    "url": "/pixaapp/static/js/runtime-main.b2d177c1.js"
+  }
+]);
